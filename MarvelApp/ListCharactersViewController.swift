2516a78b418b824598c21076515b4f8e2472f4cb
Georgina Da Luz <daluz.georgina@gmail.com>
2023-05-21 22:23:15 -0300

    Initial Commit

---
+++ MarvelApp/ListCharactersViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Gigi on 5/21/23.
+//
+
+import UIKit
+
+class ListCharactersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
