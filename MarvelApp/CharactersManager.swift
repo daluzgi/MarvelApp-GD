1b73fa74406dce2ed78d8e8f01fe5a7984ae064b
Georgina Da Luz <daluz.georgina@gmail.com>
2023-05-22 17:24:01 -0300

    Project distribution updated.

--- MarvelApp/CharactersManager.swift
+++ MarvelApp/CharactersManager.swift
@@ -10,3 +10,24 @@ import Foundation
 protocol characterManagerDelegate {
     func displayListCharacters(list: [Character])
 }
+
+struct CharactersManager {
+    var delegate: characterManagerDelegate?
+    
+    func displayCharacter() {
+        let urlString = " "
+        
+        if let url + URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nill {
+                print("Error getting data from Api."), error?.localizedDescription)
+                }
+                if let SafeData = data.parseData(quitarString: "null,")
+                
+                
+            }
+        }
+    }
+}
