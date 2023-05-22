bd3ca33a8ce55f447ad346653f20686c14c68a47
Georgina Da Luz <daluz.georgina@gmail.com>
2023-05-22 19:16:54 -0300

    - home module updated - home cells created - collection view integration

--- MarvelApp/📱/Modules/Home/Controller/HomeViewController.swift
+++ MarvelApp/📱/Modules/Home/Controller/HomeViewController.swift
@@ -8,18 +8,55 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     let viewModel = HomeViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCollectionView()
+        loadComics()
 
+    }
+    
+    private func setupCollectionView() {
+                let nibName = UINib(nibName: "HomeCollectionViewCell", bundle: nil)
+                collectionView.register(nibName, forCellWithReuseIdentifier: "HomeCollectionViewCell")
+                
+        //        collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: "HomeCollectionViewCell")
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
     
     func loadComics() {
         viewModel.loadComics()
+        
+    }
+}
+
+//MARK: - CollectionView
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCollectionViewCell", for: indexPath)
 
+//        let data = data[indexPath.item]
+//        cell.textLabel.text = String(data)
+        return cell
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //Do whatever you need when the user taps on the cell
+    }
 }
--- MarvelApp/📱/Modules/Home/Models/CharacterModel.swift
+++ MarvelApp/📱/Modules/Home/Models/CharacterModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Character: Decodable, Identifiable {
+struct CharacterModel: Decodable, Identifiable {
     let id: Int
     let name: String
     let description: String
